ae904f5caadca8340dbf979150728412655dbbfa
Roman Podymov <podymfrombryansk@yandex.com>
2024-06-04 16:53:41 +0200

    Buttons

--- Calcium/MainScreen.swift
+++ Calcium/MainScreen.swift
@@ -9,7 +9,38 @@
 import SwiftUI
 
 struct MainScreen: View {
+    @State
+    var displayingText = ""
+
+    private func button(for text: String) -> some View {
+        Button(text) {
+            displayingText += text
+        }
+        .scaledToFit()
+        .frame(idealWidth: 200)
+    }
+
     var body: some View {
-        Text("Hello")
+        VStack {
+            Text(displayingText)
+
+            HStack {
+                button(for: "1")
+                button(for: "2")
+                button(for: "3")
+            }
+
+            HStack {
+                button(for: "4")
+                button(for: "5")
+                button(for: "6")
+            }
+
+            HStack {
+                button(for: "7")
+                button(for: "8")
+                button(for: "9")
+            }
+        }
     }
 }
