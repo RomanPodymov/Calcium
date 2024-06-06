463560279ec025eb5fd4fdefb9d89d015e5e229d
Roman Podymov <podymfrombryansk@yandex.com>
2024-06-06 09:00:39 +0200

    Tests

---
+++ CalciumTests/CalciumTests.swift
@@ -0,0 +1,13 @@
+//
+//  CalciumTests.swift
+//  CalciumTests
+//
+//  Created by Roman Podymov on 06/06/2024.
+//  Copyright © 2024 Calcium. All rights reserved.
+//
+
+import XCTest
+
+class CalciumTests: XCTestCase {
+    func test() { }
+}
