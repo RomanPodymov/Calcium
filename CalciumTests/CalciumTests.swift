b557387a1dea2f5698ec672fd10a355e34d3255e
Roman Podymov <podymfrombryansk@yandex.com>
2024-06-26 12:52:53 +0200

    testState

--- CalciumTests/CalciumTests.swift
+++ CalciumTests/CalciumTests.swift
@@ -6,8 +6,19 @@
 //  Copyright © 2024 Calcium. All rights reserved.
 //
 
+@testable import CalciumApp
+import ComposableArchitecture
 import XCTest
 
 class CalciumTests: XCTestCase {
-    func test() {}
+    func testState() {
+        let store = Store(initialState: MainReducer.State()) {
+            MainReducer()
+        }
+        let screen = MainScreen(
+            store: store
+        )
+        store.send(.pressButton(.digit(.one)))
+        XCTAssertEqual(screen.store.state.displayingText, CalculatorButton.digit(.one).displayingValue)
+    }
 }
